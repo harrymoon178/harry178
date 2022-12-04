732756b26ab2af7fd7952bd315c773179d1ac4f0
harrymoon178 <114809002+harrymoon178@users.noreply.github.com>
2022-12-04 11:49:21 +0800

    Create game.h

---
+++ game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "user.h"
+
+using namespace std;
+
+void single(User &usr);
+
+bool sameCard(int card_idx);
+
+void randomCard();
+
+void printCard();
+
+#endif
