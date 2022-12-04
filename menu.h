002ace3286ecc3c59982d79056cd31377e5d9dab
harrymoon178 <114809002+harrymoon178@users.noreply.github.com>
2022-12-05 01:37:01 +0800

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,20 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "user.h"
+
+using namespace std;
+
+bool sameName(string name);
+
+void newUser();
+
+void selectUser();
+
+void printLeaderboard();
+
+void loadGame();
+
+void saveGame();
+
+#endif
