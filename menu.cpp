920944f42f234cc871180a929a3f9ed1d26b4705
harrymoon178 <114809002+harrymoon178@users.noreply.github.com>
2022-12-05 18:43:44 +0800

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -32,7 +32,6 @@ void newUser() {
   User u(name);
   users.push_back(u);
 }
-}
 
 // Function: to continue the last game
 // Input: choose a user
@@ -120,49 +119,5 @@ void saveGame() {
     for (int i = 0; i < users.size(); i++)
       fout << users[i].name << ' ' << users[i].score << ' ' << users[i].level << ' ' << users[i].money << endl;
     fout.close();
-  }
-}
-// Function: to continue the last game
-// Input: choose a user
-// Output: 
-// - if no such user: 'no player record'
-// - if invalid input: 'error' 
-// - choose the user and start the game
-void selectUser() { 
-  if (users.size() == 0)
-    cout << "\033[1;31m--Error: No player record \033[0m" << endl;
-  else {
-    cout << "Select a user: " << endl;
-    for (int i = 0; i < users.size(); i++) {
-      cout << "   " << i+1 << ". ";
-      users[i].print_info();
-    }
-
-    int u;
-    while (1) {
-      cout << ">> ";
-      cin >> u;
-      if (u <= users.size())
-        break;
-      cout << "\033[1;31m--Error: Please input a valid number \033[0m" << endl;
-    }
-    single(users[u-1]);
-  }
-}
-
-// Function: to print the outline of the leaderboard
-// Input: No input
-// Output: the outline of the leaderboard
-void printLeaderboard() {
-  if (users.size() == 0)
-    cout << "\033[1;31m--Error: No player record \033[0m" << endl;
-  else {
-    cout << "\033[1;36m === Leaderboard: === \033[0m" << endl;
-    vector<User> ranking = users;
-    sort(ranking.begin(), ranking.end());
-
-    for (int i = 0; i < users.size(); i++)
-      cout << "     " << "No." << i+1 << " " << ranking[i].name << " " << ranking[i].score << endl;
-    cout << "\033[1;36m ==================== \033[0m" << endl;
   }
 }
